f6c50cf7bba094d8cfd2ab1754e94e83255adcdd
Phant0m-123 <78161037+Phant0m-123@users.noreply.github.com>
2021-04-23 19:11:23 +0530

    Fixed launcer.py(again)

--- Laucher.py
+++ Laucher.py
@@ -2,10 +2,16 @@ from subprocess import call
 import os
 ph = os.path.join("../Aim-Trainer/Code/Game.py")
 class CallPy(object):
-    def __init__(self, path = ph):
-        self.path = path
-    def call_file(self):
-        call(["Python", "{}".format(self.path)])
+        def __init__(self, path = ph, path_2="../Aim-trainer-main/Code/Game.py"):
+            self.path = path
+            self.path_2 = path_2
+        def call_file(self):
+            call(["Python", "{}".format(self.path)])
+        def call_2(self):
+            call(["Python","{}".format(self.path_2)])
 if __name__ == "__main__":
     c = CallPy()
-    c.call_file()
+    try:
+        c.call_file()
+    except:
+        c.call_2()
