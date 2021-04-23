70e91e14bc1e9a5d944e32332b21fd10280b0c8c
Phant0m-123 <78161037+Phant0m-123@users.noreply.github.com>
2021-04-23 18:55:33 +0530

    Fixed launcher.py(i forgot a '-' lmao)

--- Laucher.py
+++ Laucher.py
@@ -1,7 +1,8 @@
 from subprocess import call
 import os
+ph = os.path.join("../Aim-Trainer/Code/Game.py")
 class CallPy(object):
-    def __init__(self, path="../Aim-Trainer/Code/Game.py"):
+    def __init__(self, path = ph):
         self.path = path
     def call_file(self):
         call(["Python", "{}".format(self.path)])
