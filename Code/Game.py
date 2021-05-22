f7a88d7d9d4f11f05d804a7d8ef179f467f65646
Phant0m-123 <78161037+Phant0m-123@users.noreply.github.com>
2021-05-22 22:29:26 +0530

    Moving to arcade (basic game created)

--- Code/Game.py
+++ Code/Game.py
@@ -1,57 +1,86 @@
 import asyncio
 import random
-import threading
-
+import time
 import arcade
-import keyboard
+from PIL import Image
 
-s_width = 1280
-s_height = 720
+
+s_width, s_height = arcade.window_commands.get_display_size()
 s_title = "Aim Trainer"
 
 
+image = Image.open("../Assets/quit_screen.png")
+img_final = image.resize((s_width,s_height))
+img_final.save('quitscreen.png')
 cursorx = 0
 cursory = 0
-targetX = random.randint(250, 1616 - 10)
-targetY = random.randint(10, 1080 - 74)
+targetX = random.randint((s_width/4)-26,(s_width/4)*3+26)
+targetY = random.randint(26,s_height-26)
 target = arcade.Sprite("../Assets/obj.png", center_x= targetX, center_y=targetY)
 cursor = arcade.Sprite("../Assets/cursor.png", center_x= 100, center_y=100)
-#collide = arcade.check_for_collision()
+quit_button = arcade.Sprite("../Assets/quit.png", center_x=(s_width/8)*7.5, center_y=s_height-50)
+quit_press = False
+score = 0
+hitsound = arcade.load_sound("../Assets/hit.mp3")
+goodbye = arcade.load_sound("../Assets/goodbye.mp3",True)
+#bye = arcade.Sprite("../Assets/quit_screen.png",center_x=(s_width/2),center_y=(s_height/2))
 
 
 def draw_objects():
+    quit_button.draw()
+    arcade.draw_text(f"Score = {score}",10,s_height-76,arcade.color.RED_ORANGE,38)
+    arcade.draw_rectangle_outline((s_width/2),(s_height/2),((s_width/4)*3),s_height,arcade.color.RED_ORANGE,10)
     target.draw()
     cursor.draw()
-
+#async def bye(a):
+    #if a == True:
+        #bye.draw()
 
 class gam(arcade.Window):
     def __init__(self, s_width, s_height, s_title):
         super().__init__(s_width, s_height, s_title, fullscreen=True, resizable=False, update_rate=1 / 165,
-                         antialiasing=False,visible=True)
+                         antialiasing=True,visible=True)
         self.set_mouse_visible(False)
         arcade.set_background_color(arcade.color.BLACK)
         self.clicked = False
+        #self.draw = False
 
     def on_draw(self):
         arcade.start_render()
         draw_objects()
+        #asyncio.run(bye(self.draw))
     def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
         cursor.center_x = x
         cursor.center_y = y
     def on_update(self, delta_time: float):
         global target, targetY,targetX
-        global cursor
+        global cursor, score, quit_press, quit_button, bye
         target.update()
         cursor.update()
         collide = arcade.check_for_collision(target,cursor)
         if collide == True and self.clicked == True:
-            target.center_y = random.randint(16, 1080 - 74)
-            target.center_x = random.randint(266, 1616 - 10)
+            hitsound.play()
+            target.center_y = random.randint(26,s_height-26)
+            target.center_x = random.randint((s_width/4)-26,(s_width/4)*3+26)
             self.clicked = False
+            score+=1
+        self.quit = arcade.check_for_collision(quit_button,cursor)
+        if self.quit == True and quit_press == True:
+            arcade.close_window()
+            goodbye.play()
+            time.sleep(4.75)
+            exit()
     def on_key_release(self, symbol, modifiers):
-        if symbol == arcade.key.Z or symbol == arcade.key.X:
-            self.clicked = True
-
+        collide = arcade.check_for_collision(target,cursor)
+        if collide == True:
+            if symbol == arcade.key.Z or symbol == arcade.key.X :
+                self.clicked = True
+    def on_mouse_press(self, x: float, y: float, button, modifiers: int):
+        global quit_press, quit_button
+        self.quit = arcade.check_for_collision(quit_button, cursor)
+        if self.quit == True:
+            if button == arcade.MOUSE_BUTTON_LEFT:
+                quit_press = True
 
 gam(s_width, s_height, s_title)
 arcade.run()
