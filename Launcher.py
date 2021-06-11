a57d42b158072dce1c476f2407b41c0ba079bb7e
Phant0m-123 <78161037+Phant0m-123@users.noreply.github.com>
2021-06-11 18:49:33 +0530

    Fixed pathing issues and some math issuses

---
+++ Launcher.py
@@ -0,0 +1,22 @@
+import os
+from subprocess import call
+from Assets import path
+
+ph = os.path.join(os.getcwd(), "Code/Game.py")
+
+
+class CallPy(object):
+    def __init__(self, path=ph, path_2="../Aim-trainer-main/Code/Game.py"):
+        self.path = path
+        self.path_2 = path_2
+
+    def call_file(self):
+        call(["Python", "{}".format(self.path)])
+
+    def call_2(self):
+        call(["Python", "{}".format(self.path_2)])
+
+
+if __name__ == "__main__":
+    c = CallPy()
+    c.call_file()
